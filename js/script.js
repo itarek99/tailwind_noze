46413cd3b9fcd40a81c6836bd3b9cc1d8c236aac
itarek99 <tarekulislam7060@gmail.com>
2022-08-08 00:46:49 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// What you can't see matters.
+const tabs = document.querySelector('.tab-btns');
+const tabBtns = document.querySelectorAll('.tab-btn');
+const tabItems = document.querySelectorAll('.tab-item');
+
+tabs.addEventListener('click', (e) => {
+  e.preventDefault();
+  const clicked = e.target.closest('.tab-btn');
+  if (!clicked) return;
+
+  // Deactivate All Items
+  tabBtns.forEach((tabBtn) => tabBtn.classList.remove('text-primaryDark', 'font-semibold'));
+  tabItems.forEach((tabItem) => tabItem.classList.add('hidden'));
+
+  // Active New Item
+  const tabData = e.target.dataset.tab;
+  clicked.classList.add('text-primaryDark', 'font-semibold');
+  document.querySelector(`.tab-item-${tabData}`).classList.remove('hidden');
+});
+
+// FAQs Accordion
+
+const accordionHeaders = document.querySelectorAll('.accordion__header');
+
+accordionHeaders.forEach((accordionHeader) => {
+  accordionHeader.addEventListener('click', (e) => {
+    const activeAccordion = document.querySelector('.accordion__header.active');
+
+    if (activeAccordion && activeAccordion !== accordionHeader) {
+      activeAccordion.classList.remove('active');
+      activeAccordion.nextElementSibling.style.maxHeight = 0;
+    }
+
+    accordionHeader.classList.toggle('active');
+    const accordionBody = accordionHeader.nextElementSibling;
+    if (accordionHeader.classList.contains('active')) {
+      accordionBody.style.maxHeight = accordionBody.scrollHeight + 'px';
+    } else {
+      accordionBody.style.maxHeight = 0;
+    }
+
+    console.log(accordionBody);
+  });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    container: {
+      padding: '0.5rem',
+    },
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+      '2xl': '1400px',
+    },
+    extend: {
+      colors: {
+        primary: '#654a9b',
+        primaryLite: '#B9B0E4',
+        primaryDark: '#210545',
+        secondary: '#6B5A78',
+      },
+      fontSize: {
+        md: '15px',
+      },
+    },
+  },
+  plugins: [],
+};
